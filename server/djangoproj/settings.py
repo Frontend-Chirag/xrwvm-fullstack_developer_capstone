82b87907ebaae10745e6425f197498aa4c087138
Frontend-Chirag <anujkashyap123000@gmail.com>
2026-07-23 10:27:01 -0400

    Added registration functionality

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -61,7 +61,11 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'frontend/static')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,9 +139,13 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static')
+    os.path.join(BASE_DIR, 'frontend/static'),
+	os.path.join(BASE_DIR, 'frontend/build'),
+	os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
 
 ALLOWED_HOSTS=['localhost','https://theianext-0-labs-prod-misc-tools-us-east-0.labs.cognitiveclass.ai:8000']
-CSRF_TRUSTED_ORIGINS=['https://theianext-0-labs-prod-misc-tools-us-east-0.labs.cognitiveclass.ai:8000']
+CSRF_TRUSTED_ORIGINS = [
+    "https://anujkashyap1-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+]
