26f751bd90d6685bd49cbdea7992bfe1ac8e3e5d
chiragkashyap__ <104261165+Frontend-Chirag@users.noreply.github.com>
2026-07-24 15:29:10 +0530

    Refine exception handling and user checks

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -67,9 +67,10 @@ def registration(request):
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
+   
+    except User.DoesNotExist:
         # If not, simply log this is a new user
-        logger.debug("{} is new user".format(username))
+        logger.debug("%s is new user", username)
 
     if not username_exist:
         # Create user in auth_user table
@@ -159,13 +160,14 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if(request.user.is_anonymous == False):
+     if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
             response = post_review(data)
             return JsonResponse({"status":200})
-        except:
-            return JsonResponse({"status":401,"message":"Error in posting review"})
+        except Exception as err:
+            logger.error(err)
+            return JsonResponse({"status": 401,"message": "Error in posting review"})
     else:
         return JsonResponse({"status":403,"message":"Unauthorized"})
 
